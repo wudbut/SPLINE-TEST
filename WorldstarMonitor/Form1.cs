58a100536427d72cad8b6e267912c7f0471ac882
wudbut <91909913+wudbut@users.noreply.github.com>
2020-07-26 13:32:15 +0000

    tty: n_gsm: Fix for modems with brk in modem status control

--- WorldstarMonitor/Form1.cs
+++ WorldstarMonitor/Form1.cs
@@ -21,4 +21,47 @@ using System.Reflection;
 
 namespace WorldStarMonitor
 {
-    p
+    public partial class Form1 : Form
+    {
+        public static string satoshify(string tobesatoshied)
+        {
+            StringBuilder str = new StringBuilder(tobesatoshied);
+            //If the number is even satoshiable
+            if (tobesatoshied.Count() > 7)
+            {
+                int indexOfPoint = tobesatoshied.Count() - 8;
+                str.Insert(indexOfPoint, '.');
+            }
+            return str.ToString();
+        }
+        System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Make donate box readonly
+            donateTextBox.ReadOnly = true;
+
+            //Load in all the 3 files, Market List, Coin List, and total List
+            int counter = 0;
+            string line;
+            // Read the file and display it line by line.
+            System.IO.StreamReader file = new System.IO.StreamReader("marketlist.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                this.marketListBox.Items.Add(line.ToString());
+                counter++;
+            }
+            file.Close();
+
+            this.marketTotalListBox.Items.Clear();
+
+            System.IO.StreamReader filec = new System.IO.StreamReader("coinlist.txt");
+            while ((line = filec.ReadLine()) != null)
+            {
+                this.marketTotalListBox.Items.Add(line.ToString());
+                counter++;
+            }
+            filec.Close();
+
+            System.IO.StreamReader
