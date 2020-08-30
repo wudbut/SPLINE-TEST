f6823d700e4e3bfffc4fe43636f2f97cb31be88a
wudbut <91909913+wudbut@users.noreply.github.com>
2020-08-30 08:47:24 +0000

    Add requirements

--- WorldstarMonitor/Form1.cs
+++ WorldstarMonitor/Form1.cs
@@ -823,4 +823,44 @@ namespace WorldStarMonitor
             public string primarycode { get; set; }
             public string secondaryname { get; set; }
             public string secondarycode { get; set; }
-            public objec
+            public object recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public object buyorders { get; set; }
+        }
+
+        public class ADTLTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+
+        public class ANCLTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+
+        public class ASCLTC
+    
