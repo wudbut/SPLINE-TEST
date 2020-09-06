2f3e253a3404b0947e6ec79e8a4782f0b25ad68a
wudbut <91909913+wudbut@users.noreply.github.com>
2020-09-06 12:30:00 +0000

    Resolve merge conflict

--- WorldstarMonitor/Form1.cs
+++ WorldstarMonitor/Form1.cs
@@ -1003,4 +1003,41 @@ namespace WorldStarMonitor
             public string secondarycode { get; set; }
             public List<Recenttrade> recenttrades { get; set; }
             public List<Sellorder> sellorders { get; set; }
-            public List<
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+        public class DBLLTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+        public class DGCLTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+        public class DIMELTC
+        {
+            public string marketid { get;
