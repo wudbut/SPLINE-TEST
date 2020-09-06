40cb214e76f13b13a2ee7c06509648c8da8f369c
wudbut <91909913+wudbut@users.noreply.github.com>
2020-09-06 14:01:16 +0000

    Removed some debug output

--- WorldstarMonitor/Form1.cs
+++ WorldstarMonitor/Form1.cs
@@ -1040,4 +1040,38 @@ namespace WorldStarMonitor
 
         public class DIMELTC
         {
-            public string marketid { get;
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+        public class DMCLTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+        public class DOGELTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string 
