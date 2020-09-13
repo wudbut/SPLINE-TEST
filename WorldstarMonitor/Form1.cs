850ddcc2f3aa7abb0e0ae1469fe0a2c36debe8c6
wudbut <91909913+wudbut@users.noreply.github.com>
2020-09-13 15:20:12 +0000

    Add more docs

--- WorldstarMonitor/Form1.cs
+++ WorldstarMonitor/Form1.cs
@@ -1177,4 +1177,39 @@ namespace WorldStarMonitor
             public string primarycode { get; set; }
             public string secondaryname { get; set; }
             public string secondarycode { get; set; }
-            public List<Recenttrade> 
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+        public class FSTLTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+        public class FTCLTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+   
