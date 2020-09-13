ba4106e322e095c2a451fbbe151dfbe31f230f81
wudbut <91909913+wudbut@users.noreply.github.com>
2020-09-13 18:29:12 +0000

    Update courses

--- WorldstarMonitor/Form1.cs
+++ WorldstarMonitor/Form1.cs
@@ -1212,4 +1212,41 @@ namespace WorldStarMonitor
             public List<Recenttrade> recenttrades { get; set; }
             public List<Sellorder> sellorders { get; set; }
             public List<Buyorder> buyorders { get; set; }
-   
+        }
+
+        public class GLDLTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+        public class GMELTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+        public class IFCLTC
+        {
+            public string marketid { get; set; }
+            public string la
