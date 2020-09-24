d21dd806c5622f94f8a07c153d6c1bb0cd635300
wudbut <91909913+wudbut@users.noreply.github.com>
2020-09-24 17:12:58 +0000

    align left

--- WorldstarMonitor/Form1.cs
+++ WorldstarMonitor/Form1.cs
@@ -1391,4 +1391,42 @@ namespace WorldStarMonitor
         }
 
         public class NXTLTC
-    
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+
+        public class PPCLTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+        public class PXCLTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+  
