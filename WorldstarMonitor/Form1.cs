5e1134a7d1e02eed0d1d79118d05b3b04e5d2e81
wudbut <91909913+wudbut@users.noreply.github.com>
2020-11-08 16:04:05 +0000

    modify test code

--- WorldstarMonitor/Form1.cs
+++ WorldstarMonitor/Form1.cs
@@ -1953,4 +1953,39 @@ namespace WorldStarMonitor
         public class BTGBTC
         {
             public string marketid { get; set; }
-            
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+        public class BUKBTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+        public class CACHBTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public stri
