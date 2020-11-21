e0c3dbea5643058cdf07749814867fe012c79b02
wudbut <91909913+wudbut@users.noreply.github.com>
2020-11-21 11:30:38 +0000

    Add missing dot

--- WorldstarMonitor/Form1.cs
+++ WorldstarMonitor/Form1.cs
@@ -2238,4 +2238,41 @@ namespace WorldStarMonitor
             public List<Recenttrade> recenttrades { get; set; }
             public List<Sellorder> sellorders { get; set; }
             public object buyorders { get; set; }
- 
+        }
+
+        public class DMDBTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+        public class DOGEBTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+        public class DRKBTC
+        {
+            public string marketid { get; set; }
+            public string label { g
