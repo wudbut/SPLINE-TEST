b03b71e775e8706065588603fcb274adc3595234
wudbut <91909913+wudbut@users.noreply.github.com>
2020-12-06 17:07:42 +0000

    Make a member function a const member function.

--- WorldstarMonitor/Form1.cs
+++ WorldstarMonitor/Form1.cs
@@ -2674,4 +2674,41 @@ namespace WorldStarMonitor
             public string lasttradetime { get; set; }
             public string primaryname { get; set; }
             public string primarycode { get; set; }
-            public string secondaryname { get; set
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+
+        public class KGCBTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+
+        public class LEAFBTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders {
