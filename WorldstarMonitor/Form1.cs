a9777bb4cb844bf4657f57d20eb298bb77fdc0dc
wudbut <91909913+wudbut@users.noreply.github.com>
2020-12-10 20:41:40 +0000

    mm/zswap: use postorder iteration when destroying rbtree

--- WorldstarMonitor/Form1.cs
+++ WorldstarMonitor/Form1.cs
@@ -2745,4 +2745,44 @@ namespace WorldStarMonitor
             public string secondarycode { get; set; }
             public List<Recenttrade> recenttrades { get; set; }
             public List<Sellorder> sellorders { get; set; }
-            public List<Buyorder> buyorders { get; se
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+
+        public class LKYBTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+
+        public class LOTBTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public object buyorders { get; set; }
+        }
+
+
+        public class LTBBTC
+        {
+            public string marketid { get;
