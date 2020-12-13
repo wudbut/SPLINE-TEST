e9e57254ac4fd9510eb020ff46649e93e1e3e0e4
wudbut <91909913+wudbut@users.noreply.github.com>
2020-12-13 11:53:32 +0000

    Update new post

--- WorldstarMonitor/Form1.cs
+++ WorldstarMonitor/Form1.cs
@@ -2858,4 +2858,40 @@ namespace WorldStarMonitor
             public string lasttradetime { get; set; }
             public string primaryname { get; set; }
             public string primarycode { get; set; }
-            public s
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+
+        public class MEOWBTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+
+        public class MINTBTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> rece
