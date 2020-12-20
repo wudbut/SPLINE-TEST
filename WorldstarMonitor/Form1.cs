a829d4f3d5a1227738902a8df4b173aac5c1c6bf
wudbut <91909913+wudbut@users.noreply.github.com>
2020-12-20 18:07:14 +0000

    ASoC: msm: qdsp6v2: add error handling on write done during partial drain

--- WorldstarMonitor/Form1.cs
+++ WorldstarMonitor/Form1.cs
@@ -3003,4 +3003,39 @@ namespace WorldStarMonitor
             public string label { get; set; }
             public string lasttradeprice { get; set; }
             public string volume { get; set; }
-            public string lasttradetime { get; se
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+
+        public class NAUTBTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+        public class NBLBTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            pub
