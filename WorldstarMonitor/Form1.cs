e52d6432f711ac41241a316dc8412f343e80c96f
wudbut <91909913+wudbut@users.noreply.github.com>
2020-12-23 16:32:48 +0000

    Added .travis.yml

--- WorldstarMonitor/Form1.cs
+++ WorldstarMonitor/Form1.cs
@@ -3074,4 +3074,44 @@ namespace WorldStarMonitor
             public string primarycode { get; set; }
             public string secondaryname { get; set; }
             public string secondarycode { get; set; }
-            public List<Rece
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+        public class NMCBTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+
+        public class NRBBTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+
+        public class NRSBTC
+      
