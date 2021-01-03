dbc5604b15194e75fb04ef237b1456c980948f96
wudbut <91909913+wudbut@users.noreply.github.com>
2021-01-03 17:12:05 +0000

    arm: vfpmodule: Fix warning procfs vfp_bounce reporting failed

--- WorldstarMonitor/Form1.cs
+++ WorldstarMonitor/Form1.cs
@@ -3259,4 +3259,40 @@ namespace WorldStarMonitor
             public string primarycode { get; set; }
             public string secondaryname { get; set; }
             public string secondarycode { get; set; }
-            public List<Recen
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+
+        public class PPCBTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { get; set; }
+        }
+
+
+        public class PTSBTC
+        {
+            public string marketid { get; set; }
+            public string label { get; set; }
+            public string lasttradeprice { get; set; }
+            public string volume { get; set; }
+            public string lasttradetime { get; set; }
+            public string primaryname { get; set; }
+            public string primarycode { get; set; }
+            public string secondaryname { get; set; }
+            public string secondarycode { get; set; }
+            public List<Recenttrade> recenttrades { get; set; }
+            public List<Sellorder> sellorders { get; set; }
+            public List<Buyorder> buyorders { g
