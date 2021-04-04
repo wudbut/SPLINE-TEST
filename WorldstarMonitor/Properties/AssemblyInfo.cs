f33a87daf557e83069c31f2dae6a6f1cc3b17e0a
wudbut <91909913+wudbut@users.noreply.github.com>
2021-04-04 16:53:03 +0000

    Refactor HospitalEquipment

---
+++ WorldstarMonitor/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Worldstar Monitor")]
+[assembly: AssemblyDescription("Monitors Cryptocurrencies")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Groupsulting")]
+[assembly: AssemblyProduct("Worldstar Monitor")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[ass
