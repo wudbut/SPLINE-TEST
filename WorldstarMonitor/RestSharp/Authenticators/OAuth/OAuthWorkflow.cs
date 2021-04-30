7ac6b4c1f06206c44ed157e2521355f1e01abff8
wudbut <91909913+wudbut@users.noreply.github.com>
2021-04-30 15:09:20 +0000

    fix for safari

--- WorldstarMonitor/RestSharp/Authenticators/OAuth/OAuthWorkflow.cs
+++ WorldstarMonitor/RestSharp/Authenticators/OAuth/OAuthWorkflow.cs
@@ -64,4 +64,49 @@ namespace RestSharp.Authenticators.OAuth
 		{
 			ValidateTokenRequestState();
 
-			if (parameters == nul
+			if (parameters == null)
+			{
+				parameters = new WebParameterCollection();
+			}
+
+			var timestamp = OAuthTools.GetTimestamp();
+			var nonce = OAuthTools.GetNonce();
+
+			AddAuthParameters(parameters, timestamp, nonce);
+
+			var signatureBase = OAuthTools.ConcatenateRequestElements(method, RequestTokenUrl, parameters);
+			var signature = OAuthTools.GetSignature(SignatureMethod, SignatureTreatment, signatureBase, ConsumerSecret);
+
+			var info = new OAuthWebQueryInfo
+			{
+				WebMethod = method,
+				ParameterHandling = ParameterHandling,
+				ConsumerKey = ConsumerKey,
+				SignatureMethod = SignatureMethod.ToRequestValue(),
+				SignatureTreatment = SignatureTreatment,
+				Signature = signature,
+				Timestamp = timestamp,
+				Nonce = nonce,
+				Version = Version ?? "1.0",
+				Callback = OAuthTools.UrlEncodeRelaxed(CallbackUrl ?? ""),
+				TokenSecret = TokenSecret,
+				ConsumerSecret = ConsumerSecret
+			};
+
+			return info;
+		}
+
+		/// <summary>
+		/// Generates a <see cref="OAuthWebQueryInfo"/> instance to pass to an
+		/// <see cref="IAuthenticator" /> for the purpose of exchanging a request token
+		/// for an access token authorized by the user at the Service Provider site.
+		/// </summary>
+		/// <param name="method">The HTTP method for the intended request</param>
+		/// <seealso cref="http://oauth.net/core/1.0#anchor9"/>
+		public virtual OAuthWebQueryInfo BuildAccessTokenInfo(string method)
+		{
+			return BuildAccessTokenInfo(method, null);
+		}
+
+		/// <summary>
+		/// Generates a <see cref="OAuthWe
