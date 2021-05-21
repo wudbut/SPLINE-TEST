ba466bac7bcb2dec7b86531708e6531363d14941
wudbut <91909913+wudbut@users.noreply.github.com>
2021-05-21 06:50:33 +0000

    inetpeer: fix a race in inetpeer_gc_worker()

--- WorldstarMonitor/RestSharp/Compression/ZLib/ZLib.cs
+++ WorldstarMonitor/RestSharp/Compression/ZLib/ZLib.cs
@@ -150,3 +150,35 @@ namespace RestSharp.Compression.ZLib
         /// <summary>Reads a number of characters from the current source Stream and writes the data to the target array at the specified index.</summary>
         /// <param name="sourceStream">The source Stream to read from.</param>
         /// <param name="target">Contains the array of characteres read from the source Stream.</param>
+        /// <param name="start">The starting index of the target array.</param>
+        /// <param name="count">The maximum number of characters to read from the source Stream.</param>
+        /// <returns>The number of characters read. The number will be less than or equal to count depending on the data available in the source Stream. Returns -1 if the end of the stream is reached.</returns>
+        public static System.Int32 ReadInput(System.IO.Stream sourceStream, byte[] target, int start, int count)
+        {
+            // Returns 0 bytes if not enough space in target
+            if (target.Length == 0)
+                return 0;
+
+            if (count == 0) return 0;
+
+            // why double-buffer?
+            //byte[] receiver = new byte[target.Length];
+            int bytesRead = sourceStream.Read(target, start, count);
+
+            // Returns -1 if EOF
+            //if (bytesRead == 0)
+            //    return -1;
+
+            //for (int i = start; i < start + bytesRead; i++)
+            //    target[i] = (byte)receiver[i];
+
+            return bytesRead;
+        }
+#endif
+
+
+		/// <summary>Reads a number of characters from the current source TextReader and writes the data to the target array at the specified index.</summary>
+		/// <param name="sourceTextReader">The source TextReader to read from</param>
+		/// <param name="target">Contains the array of characteres read from the source TextReader.</param>
+		/// <param name="start">The starting index of the target array.</param>
+		/// 
