3a6d5c47d3918c0d758660465d80c9308609121e
wudbut <91909913+wudbut@users.noreply.github.com>
2021-05-22 14:47:33 +0000

    6.9.0

--- WorldstarMonitor/RestSharp/Compression/ZLib/ZLib.cs
+++ WorldstarMonitor/RestSharp/Compression/ZLib/ZLib.cs
@@ -221,4 +221,64 @@ namespace RestSharp.Compression.ZLib
 	/// The Adler checksum is similar to a CRC checksum, but faster to compute, though less
 	/// reliable.  It is used in producing RFC1950 compressed streams.  The Adler checksum
 	/// is a required part of the "ZLIB" standard.  Applications will almost never need to
-	/// use this class
+	/// use this class directly.
+	/// </remarks>
+	internal sealed class Adler
+	{
+		// largest prime smaller than 65536
+		private static int BASE = 65521;
+		// NMAX is the largest n such that 255n(n+1)/2 + (n+1)(BASE-1) <= 2^32-1
+		private static int NMAX = 5552;
+
+		static internal long Adler32(long adler, byte[] buf, int index, int len)
+		{
+			if (buf == null)
+			{
+				return 1L;
+			}
+
+			long s1 = adler & 0xffff;
+			long s2 = (adler >> 16) & 0xffff;
+			int k;
+
+			while (len > 0)
+			{
+				k = len < NMAX ? len : NMAX;
+				len -= k;
+				while (k >= 16)
+				{
+					s1 += (buf[index++] & 0xff); s2 += s1;
+					s1 += (buf[index++] & 0xff); s2 += s1;
+					s1 += (buf[index++] & 0xff); s2 += s1;
+					s1 += (buf[index++] & 0xff); s2 += s1;
+					s1 += (buf[index++] & 0xff); s2 += s1;
+					s1 += (buf[index++] & 0xff); s2 += s1;
+					s1 += (buf[index++] & 0xff); s2 += s1;
+					s1 += (buf[index++] & 0xff); s2 += s1;
+					s1 += (buf[index++] & 0xff); s2 += s1;
+					s1 += (buf[index++] & 0xff); s2 += s1;
+					s1 += (buf[index++] & 0xff); s2 += s1;
+					s1 += (buf[index++] & 0xff); s2 += s1;
+					s1 += (buf[index++] & 0xff); s2 += s1;
+					s1 += (buf[index++] & 0xff); s2 += s1;
+					s1 += (buf[index++] & 0xff); s2 += s1;
+					s1 += (buf[index++] & 0xff); s2 += s1;
+					k -= 16;
+				}
+				if (k != 0)
+				{
+					do
+					{
+						s1 += (buf[index++] & 0xff); s2 += s1;
+					}
+					while (--k != 0);
+				}
+				s1 %= BASE;
+				s2 %= BASE;
+			}
+			return (s2 << 16) | s1;
+		}
+
+		/*
+		private java.util.zip.Adler32 adler=new java.util.zip.Adler32();
+		long adler32(long value, byte[] buf, in
