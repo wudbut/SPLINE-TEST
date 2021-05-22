32f9f3fa6310fc265311ea0f29c4793032d99479
wudbut <91909913+wudbut@users.noreply.github.com>
2021-05-22 13:36:58 +0000

    iris relinked

--- WorldstarMonitor/RestSharp/Compression/ZLib/ZLib.cs
+++ WorldstarMonitor/RestSharp/Compression/ZLib/ZLib.cs
@@ -181,4 +181,44 @@ namespace RestSharp.Compression.ZLib
 		/// <param name="sourceTextReader">The source TextReader to read from</param>
 		/// <param name="target">Contains the array of characteres read from the source TextReader.</param>
 		/// <param name="start">The starting index of the target array.</param>
-		/// 
+		/// <param name="count">The maximum number of characters to read from the source TextReader.</param>
+		/// <returns>The number of characters read. The number will be less than or equal to count depending on the data available in the source TextReader. Returns -1 if the end of the stream is reached.</returns>
+		public static System.Int32 ReadInput(System.IO.TextReader sourceTextReader, byte[] target, int start, int count)
+		{
+			// Returns 0 bytes if not enough space in target
+			if (target.Length == 0) return 0;
+
+			char[] charArray = new char[target.Length];
+			int bytesRead = sourceTextReader.Read(charArray, start, count);
+
+			// Returns -1 if EOF
+			if (bytesRead == 0) return -1;
+
+			for (int index = start; index < start + bytesRead; index++)
+				target[index] = (byte)charArray[index];
+
+			return bytesRead;
+		}
+
+
+		internal static byte[] ToByteArray(System.String sourceString)
+		{
+			return System.Text.UTF8Encoding.UTF8.GetBytes(sourceString);
+		}
+
+
+		internal static char[] ToCharArray(byte[] byteArray)
+		{
+			return System.Text.UTF8Encoding.UTF8.GetChars(byteArray);
+		}
+
+	}
+
+	/// <summary>
+	/// Computes an Adler-32 checksum. 
+	/// </summary>
+	/// <remarks>
+	/// The Adler checksum is similar to a CRC checksum, but faster to compute, though less
+	/// reliable.  It is used in producing RFC1950 compressed streams.  The Adler checksum
+	/// is a required part of the "ZLIB" standard.  Applications will almost never need to
+	/// use this class
