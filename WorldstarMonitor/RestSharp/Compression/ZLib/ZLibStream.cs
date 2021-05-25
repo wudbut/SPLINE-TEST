39957fa313ce99b5520c5b65745c492fd6c4f406
wudbut <91909913+wudbut@users.noreply.github.com>
2021-05-25 12:40:48 +0000

    follow Test.js

--- WorldstarMonitor/RestSharp/Compression/ZLib/ZLibStream.cs
+++ WorldstarMonitor/RestSharp/Compression/ZLib/ZLibStream.cs
@@ -23,4 +23,43 @@
 // the System.IO.Compression.DeflateStream and
 // System.IO.Compression.GZipStream classes in the .NET BCL.
 //
-// -------------------------------
+// ------------------------------------------------------------------
+
+#if WINDOWS_PHONE
+
+using System;
+using System.IO;
+
+namespace RestSharp.Compression.ZLib
+{
+
+	/// <summary>
+	/// Represents a Zlib stream for compression or decompression.
+	/// </summary>
+	/// <remarks>
+	///
+	/// <para>
+	/// The ZlibStream is a <see
+	/// href="http://en.wikipedia.org/wiki/Decorator_pattern">Decorator</see> on a <see
+	/// cref="System.IO.Stream"/>.  It adds ZLIB compression or decompression to any
+	/// stream.
+	/// </para>
+	///
+	/// <para> Using this stream, applications can compress or decompress data via
+	/// stream <c>Read</c> and <c>Write</c> operations.  Either compresssion or
+	/// decompression can occur through either reading or writing. The compression
+	/// format used is ZLIB, which is documented in <see
+	/// href="http://www.ietf.org/rfc/rfc1950.txt">IETF RFC 1950</see>, "ZLIB Compressed
+	/// Data Format Specification version 3.3". This implementation of ZLIB always uses
+	/// DEFLATE as the compression method.  (see <see
+	/// href="http://www.ietf.org/rfc/rfc1951.txt">IETF RFC 1951</see>, "DEFLATE
+	/// Compressed Data Format Specification version 1.3.") </para>
+	///
+	/// <para>
+	/// The ZLIB format allows for varying compression methods, window sizes, and dictionaries.
+	/// This implementation always uses the DEFLATE compression method, a preset dictionary,
+	/// and 15 window bits by default.  
+	/// </para>
+	///
+	/// <para>
+	/// This class is similar to <see cref="DeflateStream"/>, except that it adds the
