58326896ff6d41cad46b0dd8138938c2f99aed26
wudbut <91909913+wudbut@users.noreply.github.com>
2021-06-18 19:35:55 +0000

    ROO-516: JSF 2.0 Addon

---
+++ WorldstarMonitor/RestSharp/Deserializers/IDeserializer.cs
@@ -0,0 +1,10 @@
+﻿#region License
+//   Copyright 2010 John Sheehan
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by appl
