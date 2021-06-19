db726a8abe73d295b533a9f88271ca6238c331b7
wudbut <91909913+wudbut@users.noreply.github.com>
2021-06-19 13:32:37 +0000

    Minor tweak

--- WorldstarMonitor/RestSharp/Extensions/MiscExtensions.cs
+++ WorldstarMonitor/RestSharp/Extensions/MiscExtensions.cs
@@ -59,4 +59,58 @@ namespace RestSharp.Extensions
 		/// <summary>
 		/// Copies bytes from one stream to another
 		/// </summary>
-		/// <param name="input">The
+		/// <param name="input">The input stream.</param>
+		/// <param name="output">The output stream.</param>
+		public static void CopyTo(this Stream input, Stream output)
+		{
+			var buffer = new byte[32768];
+			while(true)
+			{
+				var read = input.Read(buffer, 0, buffer.Length);
+				if(read <= 0)
+					return;
+				output.Write(buffer, 0, read);
+			}
+		}
+
+		/// <summary>
+		/// Converts a byte array to a string, using its byte order mark to convert it to the right encoding.
+		/// http://www.shrinkrays.net/code-snippets/csharp/an-extension-method-for-converting-a-byte-array-to-a-string.aspx
+		/// </summary>
+		/// <param name="buffer">An array of bytes to convert</param>
+		/// <returns>The byte as a string.</returns>
+		public static string AsString(this byte[] buffer)
+		{
+            if (buffer == null) return "";
+
+			// Ansi as default
+			Encoding encoding = Encoding.UTF8;
+
+#if FRAMEWORK
+			return encoding.GetString(buffer, 0, buffer.Length);
+#else
+			if (buffer == null || buffer.Length == 0)
+				return "";
+
+			/*
+				EF BB BF		UTF-8 
+				FF FE UTF-16	little endian 
+				FE FF UTF-16	big endian 
+				FF FE 00 00		UTF-32, little endian 
+				00 00 FE FF		UTF-32, big-endian 
+				*/
+
+			if (buffer[0] == 0xef && buffer[1] == 0xbb && buffer[2] == 0xbf)
+			{
+				encoding = Encoding.UTF8;
+			}
+			else if (buffer[0] == 0xfe && buffer[1] == 0xff)
+			{
+				encoding = Encoding.Unicode;
+			}
+			else if (buffer[0] == 0xfe && buffer[1] == 0xff)
+			{
+				encoding = Encoding.BigEndianUnicode; // utf-16be
+			}
+
+			using (MemoryStream stream = 
