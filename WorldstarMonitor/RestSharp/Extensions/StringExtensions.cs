54c676f342bb7d03002be11b07a1255a5261a27e
wudbut <91909913+wudbut@users.noreply.github.com>
2021-06-29 20:30:08 +0000

    minor syntax fixes

--- WorldstarMonitor/RestSharp/Extensions/StringExtensions.cs
+++ WorldstarMonitor/RestSharp/Extensions/StringExtensions.cs
@@ -2,4 +2,60 @@
 //   Copyright 2010 John Sheehan
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
-//   you may 
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License. 
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Globalization;
+
+#if SILVERLIGHT
+using System.Windows.Browser;
+#endif
+
+#if WINDOWS_PHONE
+#endif
+
+#if FRAMEWORK || MONOTOUCH || MONODROID
+using RestSharp.Contrib;
+#endif
+
+
+namespace RestSharp.Extensions
+{
+	public static class StringExtensions
+	{
+#if !PocketPC
+		public static string UrlDecode(this string input)
+		{
+			return HttpUtility.UrlDecode(input);
+		}
+#endif
+
+		/// <summary>
+		/// Uses Uri.EscapeDataString() based on recommendations on MSDN
+		/// http://blogs.msdn.com/b/yangxind/archive/2006/11/09/don-t-use-net-system-uri-unescapedatastring-in-url-decoding.aspx
+		/// </summary>
+		public static string UrlEncode(this string input)
+		{
+			const int maxLength = 32766;
+			if (input == null)
+				throw new ArgumentNullException("input");
+
+			if (input.Length <= maxLength)
+				return Uri.EscapeDataString(input);
+
+			StringBuilder sb = new StringBuilder(input.Length
