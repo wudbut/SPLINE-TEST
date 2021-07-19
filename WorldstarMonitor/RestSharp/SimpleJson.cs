3007fdf3fcfaa8317f558e512ba22aededae7afb
wudbut <91909913+wudbut@users.noreply.github.com>
2021-07-19 11:42:20 +0000

    Update schedule.

---
+++ WorldstarMonitor/RestSharp/SimpleJson.cs
@@ -0,0 +1,7 @@
+//-----------------------------------------------------------------------
+// <copyright file="SimpleJson.cs" company="The Outercurve Foundation">
+//    Copyright (c) 2011, The Outercurve Foundation.
+//
+//    Licensed under the MIT License (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obta
