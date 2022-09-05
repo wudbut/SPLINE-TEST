fedf3f57556e39ad4f626059e7f2e694860b255b
wudbut <91909913+wudbut@users.noreply.github.com>
2022-09-05 08:09:39 +0000

    Update blessing

--- packages/Newtonsoft.Json.5.0.8/tools/install.ps1
+++ packages/Newtonsoft.Json.5.0.8/tools/install.ps1
@@ -50,4 +50,44 @@ try
       [System.Reflection.BindingFlags]::NonPublic)
     if ($instanceField -eq $null -or $consoleField -eq $null) { return }
 
-    
+    $instance = $instanceField.GetValue($null)
+    if ($instance -eq $null) { return }
+
+    $consoleProvider = $consoleField.GetValue($instance)
+    if ($consoleProvider -eq $null) { return }
+
+    $console = $consoleProvider.CreateOutputConsole($false)
+
+    $messagesField = $console.GetType().GetField("_messages", [System.Reflection.BindingFlags]::Instance -bor `
+      [System.Reflection.BindingFlags]::NonPublic)
+    if ($messagesField -eq $null) { return }
+
+    $messages = $messagesField.GetValue($console)
+    if ($messages -eq $null) { return }
+
+    $operations = $messages -split "=============================="
+
+    $lastOperation = $operations | select -last 1
+
+    if ($lastOperation)
+    {
+      $lastOperation = $lastOperation.ToLower()
+
+      $lines = $lastOperation -split "`r`n"
+
+      $installMatch = $lines | ? { $_.StartsWith("------- installing...newtonsoft.json ") } | select -first 1
+
+      if ($installMatch)
+      {
+        $dte2.ItemOperations.Navigate($url) | Out-Null
+      }
+    }
+  }
+}
+catch
+{
+  # stop potential errors from bubbling up
+  # worst case the splash page won't open
+}
+
+# yolo
